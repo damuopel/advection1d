61bda9bdc7e7cd33b3a771165d8a6b4e361473ee
David <davidmunozpellicer@gmail.com>
2020-04-05 12:58:06 +0200

    Parser

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import FEM.FEM as FEM
+import sys
 
 if __name__ == '__main__':
-    # Input User
-    eSize = 1
-    xElms = 20
-    yElms = 5
-    E = 2.1e11
-    nu = 0.3
-    thickness = 0.1
-	# Create Mesh
-    Topology,XY = FEM.Mesh(eSize,xElms,yElms)
-    D = FEM.D_Matrix(E,nu)
-    K = FEM.K_Matrix(Topology,XY,D,thickness)
-    F = FEM.F_Array(Topology,XY)
-    u = FEM.Solver(K,F,Topology,XY)
-    # Post Process Data
-    # Stresses
-    # Plot Results
-    dofs = int(XY.size/2)*2
-    xDofs = np.arange(0,dofs,2)
-    yDofs = np.arange(1,dofs,2)
-    xDisp = u[xDofs]
-    yDisp = u[yDofs]
-    disp = (xDisp**2+yDisp**2)**(0.5)
-    x = XY[0,:].reshape(yElms+1,xElms+1)
-    y = XY[1,:].reshape(yElms+1,xElms+1)
-    plt.pcolor(x,y,disp.reshape(yElms+1,xElms+1))
-    plt.colorbar()
-    plt.xlim(0,xElms*eSize)
-    plt.ylim(0,yElms*eSize)
-    plt.axis('equal')
-    plt.axis('off')
-    plt.show() 
+    fileName = sys.argv[0]
+    h = sys.argv[1]
+    xElms = sys.argv[2]
+    yElms = sys.argv[3]
+    # Create Mesh
+    # Initial Conditions
+    # Boundary Conditions
+    # Get Operators
+    # Temporal Integration (SOLVER)
+    
